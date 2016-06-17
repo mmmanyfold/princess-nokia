938a22912b2f1bab91f4f32e65411fb7c3b8ec5a
dviramontes <dviramontes@gmail.com>
2016-06-16 21:46:11 -0600

    adds new title

--- Main.js
+++ Main.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Main(props) {
     return ( <div>
-        <h1>main screen</h1>
+        <h1>main screen !!!</h1>
         <h2>savage shit yo!</h2>
     </div>)
 }
