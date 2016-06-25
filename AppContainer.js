7ee18fd880d3396b3006988ec14f264404a756a8
dviramontes <dviramontes@gmail.com>
2016-06-25 13:32:12 -0600

    persist nav, closes #14

---
+++ AppContainer.js
@@ -0,0 +1,92 @@
+import React from 'react';
+
+const menu_shop = require('./img/menu_shop.png');
+const menu_shop_ro = require('./img/menu_shop_ro.png');
+const menu_tour = require('./img/menu_tour.png');
+const menu_tour_ro = require('./img/menu_tour_ro.png');
+const menu_music = require('./img/menu_music.png');
+const menu_music_ro = require('./img/menu_music_ro.png');
+const menu_art = require('./img/menu_art.png');
+const menu_art_ro = require('./img/menu_art_ro.png');
+
+const flag = require('./img/flag.png');
+const princessNokia = require('./img/princess-nokia.png');
+
+const Rollovers = {
+    Shop: [menu_shop, menu_shop_ro],
+    Tour: [menu_tour, menu_tour_ro],
+    Music: [menu_music, menu_music_ro],
+    Art: [menu_art, menu_art_ro]
+};
+
+// main app container
+class AppContainer extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            Shop: Rollovers.Shop[0],
+            Tour: Rollovers.Tour[0],
+            Music: Rollovers.Music[0],
+            Art: Rollovers.Art[0]
+        };
+    }
+
+    rollover(type, n) {
+        this.setState({[type]: Rollovers[type][n]});
+    }
+
+    render() {
+        return (<div>
+            <nav className="navbar">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <button
+                            type="button"
+                            className="navbar-toggle collapsed"
+                            data-toggle="collapse"
+                            data-target="#main_navbar"
+                            aria-expanded="false">
+                            <img width="50px" src={flag}/>
+                        </button>
+                    </div>
+                    <div className="collapse navbar-collapse" id="main_navbar">
+                        <ul className="nav navbar-nav">
+                            <li className="active">
+                                <img className="nav_shop"
+                                     src={this.state.Shop}
+                                     onMouseOver={this.rollover.bind(this, "Shop", 1)}
+                                     onMouseOut={this.rollover.bind(this, "Shop", 0)}/>
+                            </li>
+                            <li>
+                                <img className="nav_tour"
+                                     src={this.state.Tour}
+                                     onMouseOver={this.rollover.bind(this, "Tour", 1)}
+                                     onMouseOut={this.rollover.bind(this, "Tour", 0)}/>
+                            </li>
+                            <li>
+                                <img className="nav_music"
+                                     src={this.state.Music}
+                                     onMouseOver={this.rollover.bind(this, "Music", 1)}
+                                     onMouseOut={this.rollover.bind(this, "Music", 0)}/>
+                            </li>
+                            <li>
+                                <img className="nav_art"
+                                     src={this.state.Art}
+                                     onMouseOver={this.rollover.bind(this, "Art", 1)}
+                                     onMouseOut={this.rollover.bind(this, "Art", 0)}/>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div className="content">
+                {this.props.children}
+            </div>
+            <div className="name"><img width="85%" src={princessNokia}/></div>
+            <div className="flag"><img src={flag}/></div>
+        </div>);
+    }
+}
+
+export default AppContainer;
--- routes.js
+++ routes.js
@@ -1,31 +1,14 @@
 // components
 import Splash from './Splash';
 import Main from './Main';
-// deps
+import AppContainer from './AppContainer';
 import React from 'react';
-import ReactDOM from 'react-dom';
-// utils
 import { Router, Route, IndexRoute, browserHistory} from 'react-router'
-
-// main app container
-class AppContainer extends React.Component {
-    render() {
-        return (
-            <div>
-                {this.props.children}
-            </div>
-        );
-    }
-}
 
 // router
 // * with duplicate routes for github pages
 const routes = (<Router history={browserHistory}>
     <Route path='/' component={AppContainer}>
-        <IndexRoute component={Splash}/>
-        <Route path="/main" component={Main}/>
-    </Route>
-    <Route path='/princess-nokia' component={AppContainer}>
         <IndexRoute component={Splash}/>
         <Route path="/main" component={Main}/>
     </Route>
