f1bf5d0f67ea287c0a046738c5ece0dcf663023b
dviramontes <dviramontes@gmail.com>
2016-06-25 18:10:16 -0600

    rm song binary, lets try aws s3 bucket

--- Player.js
+++ Player.js
@@ -37,7 +37,7 @@ class Player extends Component {
         const songs = R.times((i) => {
             return <Song key={i}
                          id={i}
-                         src={"./song.wav"}
+                         src={"https://s3.amazonaws.com/princess-nokia/music/song.wav?X-Amz-Date=20160626T000520Z&X-Amz-Expires=300&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Signature=f431a2540c991aebd26fcc87b72c6eb90bc42a4f709c3670726bdcd27cf2d513&X-Amz-Credential=ASIAI4WFKIBGQ55UGHUA/20160626/us-east-1/s3/aws4_request&X-Amz-SignedHeaders=Host&x-amz-security-token=FQoDYXdzEJf//////////wEaDLF8rbvx0xOr8VQAbSLHAbGHCni/goEwRnb0WX2oxxRoHR8vQJ51iNklCTCR5Tj1qVCyFJsZg7ho4QdqODVnDuuZWfT2%2BEOjp1d8QLqGoXyVntQSUDE4bukglmqRD3zAvXz4aPaY/vfX%2BDZgvP2i9etWWGQUB8nx7aIvr7nL2n1icDgMZmnBI4NIEETF0fzFGyHzC/7MQiy1RHPgws6SeQkIm5vz4vJupXFSa1dn8QB1aSTrwirAfmPF5XiY1LXcRvU8bR8GTA5dQwW/1m8kI2dqB985Uc4onYe8uwU%3D"}
                          title={ "song: " + i}
                          playing={this.state.songs[i]}
                          shush={this.shush}/>;
