3c170f5b52ddb9c0a19993d509849a1f47857096
dviramontes <dviramontes@gmail.com>
2016-06-28 12:16:22 -0600

    get song list from princess-nokia-api endpoint

--- Player.js
+++ Player.js
@@ -1,69 +1,83 @@
 import React, {Component} from 'react';
 import Song from './Song';
 import R from 'ramda';
+import axios from 'axios';
 
 // todo:
 // [x] - make only one song play at the time
-// [] - fetch songs from AWS
+// [x] - fetch songs from AWS
 
 class Player extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            count: 0,
-            songs: [],
-            current: 0
-        }
-        this.shush = this.shush.bind(this);
-        this.playNext = this.playNext.bind(this);
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			count: 0,
+			songs: [],
+			titles: [],
+			current: 0
+		}
+		this.shush = this.shush.bind(this);
+		this.playNext = this.playNext.bind(this);
+		this.songUrl = this.songUrl.bind(this);
+	}
 
-    shush(index, state) {
-        this.setState({
-            current: index,
-            songs: R.update(index, state,
-                R.times(() => false, this.state.count))
-        })
-    }
+	songUrl(name) {
+		return `http://princess-nokia.s3-website-us-east-1.amazonaws.com/${name}`;
+	}
 
-    playNext() {
-        this.setState({
-            current: this.state.current += 1,
-            songs: R.times(() => false, this.state.count)
-        });
-        let next = this.state.current % this.state.count;
-        // loop through all songs once
-        if (this.state.count !== this.state.current) {
-            this.shush(next, true, R.times(() => false, this.state.count))
-        }
-    }
+	shush(index, state) {
+		this.setState({
+			current: index,
+			songs: R.update(index, state,
+				R.times(() => false, this.state.count))
+		})
+	}
 
-    componentWillMount() {
-        // probably call AWS S3 bucket here..
-        const c = 5;
-        this.setState({
-            count: c,
-            // * initialize all as not playing
-            songs: R.times(() => false, c)
-        });
-    }
+	playNext() {
+		this.setState({
+			current: this.state.current += 1,
+			songs: R.times(() => false, this.state.count)
+		});
+		let next = this.state.current % this.state.count;
+		// loop through all songs once
+		if (this.state.count !== this.state.current) {
+			this.shush(next, true, R.times(() => false, this.state.count))
+		}
+	}
 
-    render() {
-        const songs = R.times((i) => {
-            return <Song key={i}
-                         id={i}
-                         src={"http://princess-nokia.s3-website-us-east-1.amazonaws.com/music/song2.wav"}
-                         title={ "song: " + i}
-                         playing={this.state.songs[i]}
-                         shush={this.shush}
-                         onEnd={this.playNext}/>;
-        }, this.state.count);
+	componentWillMount() {
+		axios.get('http://princess-nokia-api.apps.aterial.org/api/music')
+			.then(res => {
+				const data = res.data['princess-nokia'];
+				const count = data.length;
+				this.setState({
+					count,
+					titles: R.pluck('key')(data),
+					// * initialize all as not playing
+					songs: R.times(() => false, count)
+				});
+			})
+			.catch(err => console.log(err));
+	}
 
-        return (<div className="Player">
-            {songs}
-        </div>);
-    }
+
+
+	render() {
+		const songs = R.times((i) => {
+			return <Song key={i}
+			             id={i}
+			             src={this.songUrl(this.state.titles[i])}
+			             title={this.state.titles[i]}
+			             playing={this.state.songs[i]}
+			             shush={this.shush}
+			             onEnd={this.playNext}/>;
+		}, this.state.count);
+
+		return (<div className="Player">
+			{songs}
+		</div>);
+	}
 }
 
 export default Player;
