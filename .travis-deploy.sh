4b118b7541609094bdd92388193924d4fd215e33
dviramontes <dviramontes@gmail.com>
2016-07-13 19:22:11 -0600

    deploy to s3 instead

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -13,22 +13,22 @@ then
   exit 0
 fi
 
-rev=$(git rev-parse --short HEAD)
+#rev=$(git rev-parse --short HEAD)
 
 cd dist
 
-git init
-git config user.name "dviramontes"
-git config user.email "dviramontes@gmail.com"
+#git init
+#git config user.name "dviramontes"
+#git config user.email "dviramontes@gmail.com"
+#
+#git remote add upstream "https://${GH_TOKEN}@github.com/mmmanyfold/princess-nokia.git"
+#git fetch upstream
+#git reset upstream/gh-pages
 
-git remote add upstream "https://${GH_TOKEN}@github.com/mmmanyfold/princess-nokia.git"
-git fetch upstream
-git reset upstream/gh-pages
+#echo "princessnokia.org" > CNAME
 
-# echo "mmmanyfold/princess-nokia" > CNAME
+#touch .
 
-touch .
-
-git add -A .
-git commit -m "rebuild pages at ${rev}"
-git push -q upstream HEAD:gh-pages
+#git add -A .
+#git commit -m "rebuild pages at ${rev}"
+#git push -q upstream HEAD:gh-pages
