2a41186cf7ea04e01f9e845f0eab9ee1e616e7ee
dviramontes <dviramontes@gmail.com>
2016-07-29 18:48:20 -0600

    adds images, begins splash animation

--- views/Splash.js
+++ views/Splash.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router';
-var nokia1 = require('../img/Nokia-1.jpg');
+import $ from 'jquery';
 
-function Splash() {
-    return (<div className="splash">
-      <Link to="site"><img height="100%" src={nokia1} alt="Princess Nokia"/></Link>
-    </div>);
+const splash = require('../img/splash/splash.jpg');
+const car = require('../img/splash/car.png');
+
+class Splash extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            carLeftPosition: 0
+        };
+    }
+
+    componentDidMount() {
+        $(window).scroll(e => {
+            // trigger event here
+            this.setState({
+                carLeftPosition: $('body').scrollLeft()
+            });
+        })
+    }
+
+    render() {
+        const carOffset = 100;
+        return (<div className="splash">
+            <img id="splash-img" height="100%" src={splash}/>
+            <img id="splash-car" height="10%" src={car}
+                 style={{left: this.state.carLeftPosition + carOffset}}/>
+        </div>);
+    }
 }
 
 export default Splash
